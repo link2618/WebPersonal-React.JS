7a7242743093bb110c9b54cc2ef9ea95eab5b23d
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2021-07-16 19:05:59 -0500

    Se crea archivo de estilos para projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
 import { Title } from '../Title/Title'
+import { Project } from './Project'
 
 const Projects = () => {
     return (
         <>
             <Title texto={'Projectos'} icono={'flaticon/png/013-projects.png'} autor={'Icono realizado por Freepik de www.flaticon.com'} />
+            <div className="container">
+                <Project />
+            </div>
         </>
     )
 }
--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -3,9 +3,13 @@ import ReactTooltip from 'react-tooltip'
 
 export const Title = ({texto='texto', icono, autor}) => {
 
+    const handleTitles = () => {
+        console.log(autor)
+    }
+
     return (
         <div className='titulos'>
-            <img className="icono" src={icono} alt={autor} data-tip data-for="TooltipTitle" />
+            <img className="icono" src={icono} alt={autor} data-tip data-for="TooltipTitle" onClick={handleTitles} />
             <h4> {texto.toUpperCase()} </h4>
 
             <ReactTooltip id="TooltipTitle" place="top" type="info" effect="solid">
