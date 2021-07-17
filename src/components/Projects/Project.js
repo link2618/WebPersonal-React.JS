bb98699b84edf1a20a0f223244a962bbf164634f
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2021-07-17 12:05:06 -0500

    Se crea diseño de projects

--- src/components/Projects/Project.js
+++ src/components/Projects/Project.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-export const Project = () => {
+export const Project = ({img, msg, link}) => {
     return (
         <div>
             <div className="card z-depth-2">
-                <div className="card-content">
-                    contenido
+                <div className="card-content superiorProject">
+                    <span className='title-project'>{msg}</span>
+                    <img alt={msg} className="responsive-img" src={img} />
                 </div>
                 <div className="card-action">
-                    Accion
+                    <div className="botonconf">
+                        <a href={link} className="btn waves-effect" target="_blank" rel="noopener noreferrer">Ver</a>
+                    </div>
                 </div>
             </div>
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,13 +4,27 @@ import { Title } from '../shared/Title/Title'
 import { Project } from './Project'
 
 const Projects = () => {
+
+    const data = [
+        {
+            img: 'img/mrRapido.png',
+            msg: 'Mr Rapido',
+            link: 'https://play.google.com/store/apps/details?id=com.designocana.mrrapido&hl=es_CO&gl=US'
+        },
+        {
+            img: 'img/Salud_Estetica.png',
+            msg: 'Salud y Estética',
+            link: 'https://saludyesteticaclinicaodontologica.com.co'
+        },
+    ]
+
     return (
         <>
             <Title texto={'Projectos'} icono={'flaticon/png/013-projects.png'} autor={'Icono realizado por Freepik de www.flaticon.com'} id={'TooltipProjectTitle'} />
             <div className="container sectionProjects">
-                <Project />
-                <Project />
-                <Project />
+                { data.map( item => (
+                    <Project img={item.img} msg={item.msg} link={item.link} key={item.msg} />
+                )) }
             </div>
         </>
     )
