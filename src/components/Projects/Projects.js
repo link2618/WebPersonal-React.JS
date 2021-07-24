bd8e328b12398c7d04c4f3bf0e032d6a51f0cc40
Hugo Eduardo Carvajalino Solano <link_2618@hotmail.com>
2021-07-24 17:43:39 -0500

    Se quitan animaciones

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,39 +1,40 @@
-import React, { useRef, useEffect } from 'react'
-import { gsap } from 'gsap'
+// import React, { useRef, useEffect } from 'react'
+import React from 'react'
+// import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
 import { Title } from '../shared/Title/Title'
 import { Project } from './Project'
 
-gsap.registerPlugin(ScrollTrigger)
+// gsap.registerPlugin(ScrollTrigger)
 
 const Projects = () => {
 
     // const projectRef = useRef(null)
-    const projectRefs = useRef([])
-    projectRefs.current = []
+    // const projectRefs = useRef([])
+    // projectRefs.current = []
 
-    useEffect(() => {
-        // gsap.from(projectRef.current, {duration: 1, autoAlpha: 0, ease: 'none', delay: 1})
+    // useEffect(() => {
+    //     // gsap.from(projectRef.current, {duration: 1, autoAlpha: 0, ease: 'none', delay: 1})
 
-        projectRefs.current.forEach((element, index) => {
-            gsap.fromTo(element, {
-                autoAlpha: 0
-            }, 
-            {
-                duration: 1,
-                autoAlpha: 1, 
-                easy: 'none', 
-                scrollTrigger: {
-                    id: `section-${index+1}`, 
-                    trigger: element, 
-                    start: 'top center+=100', 
-                    toggleActions: 'play none none reverse', 
-                    markers: true
-                }
-            })
-        });
-    }, [])
+    //     projectRefs.current.forEach((element, index) => {
+    //         gsap.fromTo(element, {
+    //             autoAlpha: 0
+    //         }, 
+    //         {
+    //             duration: 1,
+    //             autoAlpha: 1, 
+    //             easy: 'none', 
+    //             scrollTrigger: {
+    //                 id: `section-${index+1}`, 
+    //                 trigger: element, 
+    //                 start: 'top center+=100', 
+    //                 toggleActions: 'play none none reverse', 
+    //                 markers: true
+    //             }
+    //         })
+    //     });
+    // }, [])
 
     const data = [
         {
@@ -48,20 +49,21 @@ const Projects = () => {
         },
     ]
 
-    const addToRefs = (e) => {
-        if (e && !projectRefs.current.includes(e.target)) {
-            projectRefs.current.push(e)
-        }
-        console.log('-->', projectRefs.current)
-    }
+    // const addToRefs = (e) => {
+    //     if (e && !projectRefs.current.includes(e.target)) {
+    //         projectRefs.current.push(e)
+    //     }
+    //     console.log('-->', projectRefs.current)
+    // }
 
     return (
         <>
             <Title texto={'Proyectos'} icono={'flaticon/png/013-projects.png'} autor={'Icono realizado por Freepik de www.flaticon.com'} id={'TooltipProjectTitle'} />
             {/* <div className="container sectionProjects" ref={projectRef}> */}
             <div className="container sectionProjects">
+                    {/* <div className="card z-depth-2" key={item.msg} ref={addToRefs}> */}
                 { data.map( item => (
-                    <div className="card z-depth-2" key={item.msg} ref={addToRefs}>
+                    <div className="card z-depth-2" key={item.msg} >
                     <Project img={item.img} msg={item.msg} link={item.link} />
                     </div>
                 )) }
